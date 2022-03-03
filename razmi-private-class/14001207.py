985181993aeb5d96cc9b3deaf6c1ebe3aa5c87e6
hoomanjb <hoomanjavan74@gmail.com>
2022-03-03 16:56:31 +0330

    add new classes

---
+++ razmi-private-class/14001207.py
@@ -0,0 +1,42 @@
+# print('hello world')
+# # name = 'hooman'   in esme mane
+# a = 5
+# print('hellloooo') # asdasdasd
+
+# special Char !@#$%%^&(=-
+# a = 7
+# A = 7
+# first_name_of_customer_from_shopping_site = 'hooman'
+#
+# print(type(a))
+# print(type(first_name_of_customer_from_shopping_site))
+# print(type(4.5))
+
+# a = b = c = 'hello'
+# a, b, c = 1, 2, 3
+
+# ########################################
+# String
+# name = 'hello\nworld'
+# name2 = "hello"
+# name3 = """hello world
+# che khabar
+# che mikoni
+# """
+# print(name3)
+a = 'hello world'
+# print(a[0])
+# print(len(a))
+# print(a[-1])
+# print(a[:])
+# print(a[2:])
+# print(a[2:6])
+# print(a[::])
+# print(a[::-1])
+
+
+
+
+
+
+
