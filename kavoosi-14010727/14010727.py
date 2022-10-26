ea64224d5e757d2fb0d89a066a67fb9bd2d70668
hoomanjb <hoomanjavan74@gmail.com>
2022-10-26 17:58:46 +0330

    add 14001227

---
+++ kavoosi-14010727/14010727.py
@@ -0,0 +1,65 @@
+# Data Type:
+a = 10  # integer
+b = '10'
+c = []
+d = ()
+e = {}
+f = {'1': 1}
+v = True  # 1    False -> 0
+
+# condition if
+# match - case
+# a = '1'
+# match a:
+#     case '1':
+#         print(a)
+#     case '2':
+#         print('ss')
+
+
+# loop -> (For - while)
+
+# while a < 10:
+#     pass
+#
+# for item in b:
+#     pass
+
+
+# function
+# arguments a, b
+# def tekrari(a, b, *args, **kwargs):
+#     pass
+
+# parametr
+# tekrari(4, 6)
+
+# DRY - Dont Reapet Yourself
+
+# oop , object oriented programing
+
+# class Ensan:
+#     pass
+
+# def test(a, b, *args, **kwargs):
+#     print(args)
+#     print(kwargs)
+
+# def test2():
+#     pass
+#
+# test2(12, a=10, b=11)
+
+# test(5, 6, 7, 8, 9, 10, 11)
+# test(a=10, b=13, c=11, d=111)
+
+
+# class Ensan:
+#     pass
+
+# native
+# select * from
+# Orm
+
+# interperetri -> python
+# compileri -> go
