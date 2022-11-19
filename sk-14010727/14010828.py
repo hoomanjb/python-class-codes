d63b7b57e7d981f64f91c076c916e890659bf321
hoomanjb <hoomanjavan74@gmail.com>
2022-11-19 19:50:02 +0330

    add 1400828.py

---
+++ sk-14010727/14010828.py
@@ -0,0 +1,79 @@
+# set
+# my_set = {'apple', 'banana', 'cherry', 'apple'}
+# print(my_set)
+
+# a = [3, 4, 6, 2, 4, 5, 5, 1, 2, 3]
+# print(list(set(a)))
+
+# for item in my_set:
+#     print(item)
+
+# print('apple' in my_set)
+
+# my_set.add('orange')
+# print(my_set)
+
+# my_set = {'apple', 'banana', 'cherry', 'apple'}
+# b = {1, 3, 4, 6, 'apple'}
+# my_set.update(b)
+# print(my_set)
+# my_set.remove(100)
+# my_set.discard('asdasd')
+# my_set.pop()
+# print(my_set)
+# my_set.clear()
+
+
+# a = {'a', 'b', 'c'}
+# b = {1, 2, 3}
+# c = a.union(b, {5, 6}, {8, 9})
+#
+# print(a)
+# print(b)
+# print(c)
+
+
+# a = {'a', 'b', 'c'}
+# b = {'d', 'e', 'a'}
+
+# a.intersection_update(b)
+# print(a)
+# d = a.intersection(b)
+# print(d)
+# a.symmetric_difference_update(b)
+# print(a)
+
+# ######################################################
+# Dict
+# my_dict = {
+#     'name': 'ali',
+#     'age': 20,
+#     1: 'hello',
+#     3: 'byebye',
+#     'key': [0, 1],
+#     'True': False
+# }
+# print(my_dict['name'])
+# name
+# print(my_dict.get('True'))
+# print(my_dict.get('chert', False))
+
+# print(my_dict.keys())
+# print(my_dict.values())
+
+# my_dict['chert'] = 10
+# print(my_dict)
+#
+# print(my_dict.items())
+
+# for key, value in my_dict.items():
+#     print(f'key: {key}   value: {value}')
+
+
+# if 'name' in my_dict.values():
+#     print('ok')
+#
+# my_dict.update({'111': 222})
+# print(my_dict)
+
+# ###############################################
